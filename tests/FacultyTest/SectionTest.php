bdfff3cd2ad355f68f0ac1d38c5948cdcb3368e7
ailenepequit <ailene.pequit@gmail.com>
2016-02-02 14:51:13 +0800

    added test for section class

--- tests/FacultyTest/SectionTest.php
+++ tests/FacultyTest/SectionTest.php
@@ -12,5 +12,9 @@ class SectionTest extends PHPUnit_Framework_TestCase
 		$section = new Section();
 		$section->setSectionCode("ailene");
 		$this->assertEquals("ailene", $section->getSectionCode());
+		$section->setCourseCode("IT 109");
+		$this->assertEquals("IT 109", $section->getCourseCode());
+		$section->setClassSize(30);
+		$this->assertEquals(30, $section->getClassSize());
 	}
 }
