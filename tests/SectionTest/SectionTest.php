0068919e58a9d51d12d95b6e4296f1df861cab99
Bynn Jessa Yting <Bynn Jessa Yting>
2016-02-02 15:21:40 +0800

    corrected test file for section class

--- tests/SectionTest/SectionTest.php
+++ tests/SectionTest/SectionTest.php
@@ -11,10 +11,11 @@ class SectionTest extends PHPUnit_Framework_TestCase
 	{
 		$section = new Section();
 		$section->setSectionCode("ailene");
-		$this->assertEquals("ailene", $section->getSectionCode());
 		$section->setCourseCode("IT 109");
-		$this->assertEquals("IT 109", $section->getCourseCode());
 		$section->setClassSize(30);
+
+		$this->assertEquals("ailene", $section->getSectionCode());
+		$this->assertEquals("IT 109", $section->getCourseCode());
 		$this->assertEquals(30, $section->getClassSize());
 	}
 }
